2b2ce6d4b5498e10e0de77410263efc845b7b09f
Ching Yi, Chan <chingyichan.tw@gmail.com>
2019-12-07 12:22:12 +0800

    add spring project

---
+++ 02-spring-skaffold/src/main/java/twjug/lite/skaffold/SkaffoldApplication.java
@@ -0,0 +1,13 @@
+package twjug.lite.skaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldApplication.class, args);
+	}
+
+}
---
+++ 02-spring-skaffold/src/test/java/twjug/lite/skaffold/SkaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package twjug.lite.skaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
